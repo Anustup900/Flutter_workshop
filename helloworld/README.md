3bec4ffb2f0da5f6428cefef5cf1931090e79e03
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2020-09-11 23:51:20 +0530

    Add files via upload

---
+++ helloworld/README.md
@@ -0,0 +1,3 @@
+# helloworld (Demo)
+
+A simple hello world app to view the basics of Flutter
---
+++ helloworld/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello World"),
+      ),
+      body: Center(
+        child: Text("Hello World"),
+      ),
+    );
+  }
+}
